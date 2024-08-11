86268498f77b7bba479d6f92b588773b7d781afb
akkulsecond213 <akkulgautam14@outlook.com>
2024-08-12 00:54:34 +0530

    some minor changes

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ const dotenv = require('dotenv');
 const Document = require('./models/document');
 dotenv.config();
 connectDB();
+connectDB().catch(err => {
+  console.error(`Error connecting to the database: ${err.message}`);
+  process.exit(1); // Exit process if database connection fails
+});
 
 
 const io = require('socket.io')(4001, {
@@ -15,29 +19,34 @@ const io = require('socket.io')(4001, {
 const defaultValue = '';
 
 io.on('connection', (socket) => {
+  console.log('New client connected');
+
   socket.on('get-document', async (documentId) => {
-    const document = await findOrCreateDocument(documentId);
+    try {
+      const document = await findOrCreateDocument(documentId);
+      socket.join(documentId);
+      socket.emit('load-document', document.data);
 
-    socket.join(documentId);
+      socket.on('send-changes', (delta) => {
+        socket.broadcast.to(documentId).emit('receive-changes', delta);
+      });
 
-    socket.emit('load-document', document.data);
+      socket.on('save-document', async (data) => {
+        await Document.findByIdAndUpdate(documentId, { data });
+      });
+    } catch (error) {
+      console.error(`Error in get-document: ${error.message}`);
+    }
+  });
 
-    socket.on('send-changes', (delta) => {
-      socket.broadcast.to(documentId).emit('receive-changes', delta);
-    });
-
-    socket.on('save-document', async (data) => {
-      await Document.findByIdAndUpdate(documentId, { data });
-    });
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
   });
 });
 
 async function findOrCreateDocument(id) {
   if (id == null) return;
-  const document = await Document.findById(id);
+  let document = await Document.findById(id);
   if (document) return document;
   return await Document.create({ _id: id, data: defaultValue });
 }
-
-
-
